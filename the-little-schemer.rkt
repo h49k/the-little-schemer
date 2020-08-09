80514c71cd14bf7c7bede52b7fcf92f1008907e7
hos <hos@cinnamon.local>
2020-08-09 20:44:20 +0900

    scheme

--- the-little-schemer.rkt
+++ the-little-schemer.rkt
@@ -1,4 +1,6 @@
 #lang sicp
+; #lang racket
+; ; 
 
 (define atom?
     (lambda (x)
@@ -27,3 +29,129 @@
                 (member? a (cdr lat)))))))
 
 
+(define rember
+    (lambda (a lat)
+        (cond
+            ((null? lat) (quote ()))
+            ((eq? (car lat) a) (cdr lat))
+            (else (cons (car lat)
+                (rember a (cdr lat)))))))
+
+　
+(define firsts
+    (lambda (l)
+        (cond
+            ((null? l) '())
+            (else (cons (car (car l))
+                (firsts (cdr l)))))))
+
+
+(define insertR
+    (lambda (new old lat)
+        (cond
+            ((null? lat) '())
+            (else (cond
+                  ((eq? (car lat) old) 
+                   (cons old (cons new (cdr lat))))
+                  (else (cons (car lat)
+                    (insertR new old (cdr lat)))))))))
+
+(define insertL
+    (lambda (new old lat)
+        (cond
+            ((null? lat) '())
+            (else (cond
+                  ((eq? (car lat) old) 
+                   (cons new (cons old (cdr lat))))
+                  (else (cons (car lat)
+                    (insertL new old (cdr lat)))))))))
+
+(define subst
+    (lambda (new old lat)
+        (cond
+            ((null? lat) '())
+            (else (cond
+                  ((eq? (car lat) old) 
+                   (cons new (cdr lat)))
+                  (else (cons (car lat)
+                    (subst new old (cdr lat)))))))))
+
+(define subst2
+    (lambda (new o1 o2 lat)
+        (cond
+            ((null? lat) '())
+            (else (cond
+                  ((or (eq? (car lat) o1) (eq? (car lat) o2))
+                    (cons new (cdr lat)))
+                  (else (cons (car lat)
+                    (subst new o1 o2
+                        (cdr lat)))))))))
+
+(define multirember
+    (lambda (a lat)
+        (cond
+            ((null? lat) (quote ()))
+            (else
+                (cond
+                    ((eq? (car lat) a)
+                     (multirember a (cdr lat)))
+                     (else (cons (car lat)
+                        (multirember a
+                            (cdr lat)))))))))
+
+(define multiinsertR
+    (lambda (new old lat)
+        (cond
+            ((null? lat) (quote ()))
+            (else
+                (cond
+                    ((eq? (car lat) old)
+                        (cons (car lat)
+                            (cons new
+                                (multiinsertR new old 
+                                    (cdr lat)))))
+                    (else (cons (car lat)
+                        (multiinsertR new old
+                            (cdr lat)))))))))
+
+
+(define multiinsertL
+    (lambda (new old lat)
+        (cond
+            ((null? lat) (quote ()))
+            (else
+                (cond
+                    ((eq? (car lat) old)
+                        (cons new
+                            (cons old
+                                (multiinsertL new old 
+                                    (cdr lat)))))
+                    (else (cons (car lat)
+                        (multiinsertL new old
+                            (cdr lat)))))))))
+
+(define multisubst
+    (lambda (new old lat)
+        (cond
+            ((null? lat) (quote()))
+            (else (cond
+                    ((eq? (car lat) old)
+                    (cons new
+                        (multisubst new old
+                            (cdr lat))))
+                        (else (cons (car lat)
+                            (multisubst new old
+                                (cdr lat)))))))))
+
+
+(define o+
+    (lambda (n m)
+        (cond
+            ((zero? m) n)
+            (else (add1 (o+ n (sub1 m)))))))
+
+(define o-
+    (lambda (n m)
+        (cond
+            ((zero? m) n)
+            (else (sub1 (o- n (sub1 m)))))))
