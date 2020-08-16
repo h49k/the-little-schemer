0a51848cd07aab0c1971946f76d02ea1ab4ca8a3
hos <hos@cinnamon.local>
2020-08-16 09:21:21 +0900

    toryu

---
+++ list-asobi.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(define 1+
+    (lambda (n)
+        (+ n 1)))
+
+(define 1-
+    (lambda (n)
+        (- n 1)))
--- the-little-schemer.rkt
+++ the-little-schemer.rkt
@@ -34,8 +34,7 @@
         (cond
             ((null? lat) (quote ()))
             ((eq? (car lat) a) (cdr lat))
-            (else (cons (car lat)
-                (rember a (cdr lat)))))))
+            (else (cons (car lat) (rember a (cdr lat)))))))
 
 　
 (define firsts
@@ -50,11 +49,12 @@
     (lambda (new old lat)
         (cond
             ((null? lat) '())
-            (else (cond
-                  ((eq? (car lat) old) 
-                   (cons old (cons new (cdr lat))))
-                  (else (cons (car lat)
-                    (insertR new old (cdr lat)))))))))
+            (else
+                (cond
+                    ((eq? (car lat) old) 
+                        (cons old (cons new (cdr lat))))
+                    (else (cons (car lat)
+                        (insertR new old (cdr lat)))))))))
 
 (define insertL
     (lambda (new old lat)
@@ -155,3 +155,180 @@
         (cond
             ((zero? m) n)
             (else (sub1 (o- n (sub1 m)))))))
+
+(define addtup
+    (lambda (tup)
+        (cond
+            ((null? tup) 0)
+            (else
+                (o+ (car tup) (addtup (cdr tup)))))))
+
+
+(define ox
+    (lambda (n m)
+        (cond
+            ((zero? m) 0)
+            (else (o+ n (ox n (sub1 m)))))))
+
+
+(define tup+
+    (lambda (tup1 tup2)
+        (cond
+            ((null? tup1) tup2)
+            ((null? tup2) tup1)
+            (else
+                (cons (o+ (car tup1) (car tup2))
+                        (tup+
+                            (cdr tup1) (cdr tup2)))))))
+
+
+
+(define o<
+    (lambda (n m)
+        (cond
+            ((zero? m) #f) 
+            ((zero? n) #t)
+            (else
+                 (o< (sub1 n) (sub1 m))))))
+
+(define o>
+    (lambda (n m)
+        (cond
+            ((zero? n) #f) 
+            ((zero? m) #t)
+            (else
+                 (o> (sub1 n) (sub1 m))))))
+
+(define o=
+    (lambda (n m)
+        (cond
+            ((zero? m) (zero? n))
+            ((zero? n) #f)
+            (else (o= (sub1 n) (sub1 m))))))
+
+
+
+(define o^
+    (lambda (n m)
+        (cond
+            ((zero? m) 1)
+            (else (ox n (o^ n (sub1 m)))))))
+
+
+
+(define o/
+    (lambda (n m)
+        (cond
+            ((o< n m) 0)
+            (else (add1 (o/ (o- n m) m))))))
+
+(define length
+    (lambda (lat)
+        (cond
+        ((null? lat) 0)
+        (else (add1 (length (cdr lat)))))))
+
+
+(define pick
+    (lambda (n lat)
+        (cond
+            ((zero? (sub1 n)) (car lat))
+            (else (pick (sub1 n) (cdr lat))))))
+
+
+(define rempick
+    (lambda (n lat)
+        (cond
+            ((one? n) (cdr lat))
+            (else (cons (car lat)
+                (rempick (sub1 n)
+                    (cdr lat)))))))
+
+
+(define no-nums
+    (lambda (lat)
+        (cond
+            ((null? lat) (quote ()))
+            (else
+                (cond
+                    ((number? (car lat)) (no-nums (cdr lat)))
+                    (else (cons (car lat) (no-nums (cdr lat)))))))))
+
+
+(define all-nums
+    (lambda (lat)
+        (cond
+            ((null? lat) (quote ()))
+            (else (cond
+                ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+                (else (all-nums (cdr lat))))))))
+
+
+(define eqan?
+    (lambda (a1 a2)
+        (cond
+            ((and (number? a1) (number? a2))
+                (= a1 a2))
+            ((or (number? a1) (number? a2))
+                #f)
+            (else (eq? a1 a2)))))
+
+
+(define occer
+    (lambda (a lat)
+        (cond
+            ((null? lat) 0)
+            (else
+                (cond
+                    ((eq? (car lat) a) (add1 (occer a (cdr lat))))
+                    (else (occer a (cdr lat))))))))
+
+
+(define one?
+    (lambda (n)
+        (= n 1)))
+
+(define rember*
+    (lambda (a l)
+        (cond
+            ((null? l) (quote ()))
+            ((atom? (car l))
+                (cond
+                    ((eq? (car l) a)
+                        (rember* a (cdr l)))
+                        (else (cons (car l) (rember* a (car l))))))
+            (else (cons (rember* a (car l))
+                (rember* a (cdr l)))))))
+
+
+
+(define insertR*
+    (lambda (new old l)
+        (cond
+            ((null? l) (quote()))
+            ((atom? (car l))
+                (cond
+                    ((eq? (car l) old)
+                        (cons old (cons new (insertR* new old (cdr l)))))
+                    (else (cons (car l) (insertR* new old (cdr l))))))
+            (else (cons (insertR* new old (car l))
+                (insertR* new old (cdr l)))))))
+
+
+(define occer*
+    (lambda (a l)
+        (cond
+            ((null? l) 0)
+            ((atom? (car l))
+                (cond
+                    ((eq? (car l) a)
+                        (add1 (occer* a (cdr l))))
+                    (else (occer* a (car l)))))
+            (else (o+ (occer* (car l))
+                    (occer* (cdr l)))))))
+
+
+(define subst*
+    (lambda (new old l)
+        (cond
+            )))
